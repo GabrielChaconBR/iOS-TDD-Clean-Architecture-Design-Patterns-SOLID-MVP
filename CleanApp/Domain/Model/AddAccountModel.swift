79e884e7656d7514ad95200f126aeb80b55fec27
Gabriel Chacon <gabrielchaconbh@gmail.com>
2021-04-13 16:11:47 -0300

    Ensure RemoteAddAccount calls HttpClient with correct data

--- CleanApp/Domain/Model/AddAccountModel.swift
+++ CleanApp/Domain/Model/AddAccountModel.swift
@@ -1,8 +1,16 @@
 import Foundation
 
-public struct AddAccountModel {
+public struct AddAccountModel: Encodable {
+
     public let name: String
     public let email: String
     public let password: String
     public let passwordConfirmation: String
+    
+    public init(name: String, email: String, password: String, passwordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
 }
